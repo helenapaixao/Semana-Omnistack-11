9907ebc3d2b63668aaf39910b5926f687349d344
Helena Paixão <hp.helenapaixao@gmail.com>
2020-04-01 16:09:16 -0300

    backend finalizado

--- Backend/src/Controllers/IncidentController.js
+++ Backend/src/Controllers/IncidentController.js
@@ -2,25 +2,24 @@ const connection = require("../database/connection");
 
 module.exports = {
   async index(request, response) {
-    
-    const {page = 1 } = request.query;
+    const { page = 1 } = request.query;
 
-    const [count] = await connection('incidents')
-    .count()
+    const [count] = await connection("incidents").count();
     const incidents = await connection("incidents")
-    .limit(5)
-    .offset((page - 1) * 5)
-    .select("*");
+    .join('ongs', 'ongs.id', '=','incidents.ong_id')
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select(['incidents.*', 'ongs.name','ongs.email','ongs.whatsapp','ongs.city','ongs.uf']);
 
-     response.header('X-Total-Count', count['count(*)'])
+    response.header("X-Total-Count", count["count(*)"]);
 
     return response.json(incidents);
   },
 
   async create(request, response) {
     const { title, description, value } = request.body;
-    const ong_id = request.headers.authorization;
 
+    const ong_id = request.headers.authorization;
     const [id] = await connection("incidents").insert({
       title,
       description,
