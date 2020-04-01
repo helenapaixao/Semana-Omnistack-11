4a3f012fb9078cd982fd2202d1f50655b17178a0
Helena Paixão <hp.helenapaixao@gmail.com>
2020-04-01 16:03:55 -0300

    add pagination

--- Backend/src/Controllers/IncidentController.js
+++ Backend/src/Controllers/IncidentController.js
@@ -1,40 +1,53 @@
-const connection = require('../database/connection');
+const connection = require("../database/connection");
 
 module.exports = {
-async index(request,response) {
-  const incidents = await connection('incidents').select('*');
-  return response.json(incidents);
-},
+  async index(request, response) {
+    
+    const {page = 1 } = request.query;
+
+    const [count] = await connection('incidents')
+    .count()
+    const incidents = await connection("incidents")
+    .limit(5)
+    .offset((page - 1) * 5)
+    .select("*");
+
+     response.header('X-Total-Count', count['count(*)'])
+
+    return response.json(incidents);
+  },
 
   async create(request, response) {
-    const {title, description, value} = request.body;
+    const { title, description, value } = request.body;
     const ong_id = request.headers.authorization;
 
-    const [id] = await connection('incidents').insert({
+    const [id] = await connection("incidents").insert({
       title,
       description,
       value,
-      ong_id,
+      ong_id
     });
 
-    return response.json({id});
+    return response.json({ id });
   },
 
-  async delete(request,response) {
-    const {id} = request.params;
+  async delete(request, response) {
+    const { id } = request.params;
     const ong_id = request.headers.authorization;
 
-    const incident = await connection('incidents')
-    .where('id', id)
-    .select('ong_id')
-    .first();
+    const incident = await connection("incidents")
+      .where("id", id)
+      .select("ong_id")
+      .first();
 
-    if(incident.ong_id !== ong_id ){
-      return response.status(401).json({error:'Operation not permitted'})
+    if (incident.ong_id !== ong_id) {
+      return response.status(401).json({ error: "Operation not permitted" });
     }
 
-    await connection('incidents').where('id', id).delete();
+    await connection("incidents")
+      .where("id", id)
+      .delete();
 
     return response.status(204).send();
   }
-}
+};
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,22 +1,20 @@
-const express = require('express');
-const OngController = require('./Controllers/OngController');
-const IncidentController = require('./Controllers/IncidentController');
-const ProfileController = require('./Controllers/ProfileController');
-const SessionController = require('./Controllers/SessionController');
+const express = require("express");
+const OngController = require("./Controllers/OngController");
+const IncidentController = require("./Controllers/IncidentController");
+const ProfileController = require("./Controllers/ProfileController");
+const SessionController = require("./Controllers/SessionController");
 
 const routes = express.Router();
 
-routes.post('/sessions', SessionController.create);
+routes.post("/sessions", SessionController.create);
 
- routes.get('/ongs',OngController.index);
- routes.post('/ongs', OngController.create);
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.create);
 
- routes.get('/profile', ProfileController.index);
+routes.get("/profile", ProfileController.index);
 
- 
- routes.get('/incidents', IncidentController.index);
- routes.post('/incidents', IncidentController.create);
-  routes.delete('/incidents/:id', IncidentController.delete);
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
+routes.delete("/incidents/:id", IncidentController.delete);
 
-
- module.exports = routes;
+module.exports = routes;
