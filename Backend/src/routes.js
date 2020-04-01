93b35e3c7198f1ec4e54751ec73b4d2b0678070b
Helena Paixão <hp.helenapaixao@gmail.com>
2020-04-01 15:53:27 -0300

    add route session

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const OngController = require('./Controllers/OngController');
 const IncidentController = require('./Controllers/IncidentController');
 const ProfileController = require('./Controllers/ProfileController');
+const SessionController = require('./Controllers/SessionController');
 
 const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
 
  routes.get('/ongs',OngController.index);
  routes.post('/ongs', OngController.create);
@@ -13,7 +16,7 @@ const routes = express.Router();
  
  routes.get('/incidents', IncidentController.index);
  routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+  routes.delete('/incidents/:id', IncidentController.delete);
 
 
  module.exports = routes;
