26af0dfdf51358178ad298da57af8e63929dfa62
Helena Paixão <hp.helenapaixao@gmail.com>
2020-04-01 15:30:22 -0300

    add incidentController e config routes

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const OngController = require('./Controllers/OngController');
+const IncidentController = require('./Controllers/IncidentController');
 
 const routes = express.Router();
 
  routes.get('/ongs',OngController.index);
- routes.post('/ongs', OngController.create );
+ routes.post('/ongs', OngController.create )
+ 
+ routes.get('/incidents', IncidentController.index);
+ routes.post('/incidents', IncidentController.create);
 
  module.exports = routes;
