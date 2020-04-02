5cdcf95d7ca56550dda78169ff578fd8e7f21997
Helena Paixão <hp.helenapaixao@gmail.com>
2020-04-01 22:46:28 -0300

    add page profile

--- Frontend/src/pages/Register/index.js
+++ Frontend/src/pages/Register/index.js
@@ -3,7 +3,7 @@ import logoImg from "../../assets/logo.svg";
 import {Link} from 'react-router-dom'
 import {FiArrowLeft} from "react-icons/fi"
 
-// import './styles';
+import "./styles.css"
 
 export default function Register() {
   return(
@@ -13,9 +13,9 @@ export default function Register() {
           <img src={logoImg} alt="Be The Hero"/>
           <h1>Cadastro</h1>
           <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG</p>
-          <Link className="back-link" to="/register">
+          <Link className="back-link" to="/">
             <FiArrowLeft />
-            Não tenho cadastro
+            Já tenho cadastro
           </Link>
         </section>
         <form>
