fb8558d942a40a298d2f2858b9edc05a1b9e451a
Helena Paixão <hp.helenapaixao@gmail.com>
2020-04-01 23:08:46 -0300

    page profile done

--- Frontend/src/routes.js
+++ Frontend/src/routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 
 export default function Routes() {
@@ -12,6 +13,7 @@ export default function Routes() {
     <Switch>
       <Route path="/" component={Logon} exact />
       <Route path="/register" component={Register}  />
+      <Route path="/profile" component={Profile}/>
     </Switch>
     </BrowserRouter>
   )
